788698429adb1e20d2733d5c02bcc66140eec7aa
RyanBaby <342402002@qq.com>
2017-10-24 11:28:54 +0800

    create initial

---
+++ MyProj/MyProj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyProj
+//
+//  Created by 夜狼安防 on 2017/10/24.
+//  Copyright © 2017年 CTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyProj/MyProj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyProj
+//
+//  Created by 夜狼安防 on 2017/10/24.
+//  Copyright © 2017年 CTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
